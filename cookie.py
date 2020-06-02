3f99799b69ca5c299ecd5ad72f6ca1186f8b2844
Python <salah.abbas142@gmail.com>
2020-06-02 08:32:04 -0500

    cleaned up code for release

--- cookie.py
+++ cookie.py
@@ -1,52 +1,62 @@
 import requests as re
 from time import sleep
 import argparse
+from urllib.parse import urljoin
+import datetime
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 parser = argparse.ArgumentParser(description='<Required> Times cookies to check when or if they expire by replaying Burp Suite request.')
 parser.add_argument('request', type=str, help='BurpSuite request location')
-parser.add_argument('-s', '--seconds', type=int, help='Amount of seconds inbetween each request')
+parser.add_argument('-s', '--seconds', type=int, default=60, help='Amount of seconds inbetween each request')
+parser.add_argument('-nossl', action='store_true', help='Use ssl or not')
 
 def main(args):
-    exit()
+    try:
+        method, headers, data = parse(args.request)
+    except Exception as e:
+        print("Error while parsing file {}".format(args.request))
+        raise
 
-    url = 'https://application.local:5001/{}'
-    login = 'Account/Login/'
-    cq = 'Account/AnswerChallengeQuestions/'
-    manage = 'Manage/'
+    requesturl = urljoin("https://"+headers['Host'], method[1])
+    for i in range(args.seconds*100000):
+        if(method[0]):
+            r = re.get(requesturl, data=data, headers=headers, verify=args.nossl)
+            print(r.text)
+        else:
+            r = re.post(requesturl, data=data, headers=headers, verify=args.nossl)
 
-    loginData = {
-        'UserName': 'user',
-        'Password': 'asd',
-        'RemeberMe': 'false'
-    }
-
-    cqData = {
-        'Answer1': 'a',
-        'Answer2': 'a'
-    }
-
-    re.post(url.format(login), loginData, verify=False)
-    re.post(url.format(cq), cqData, verify=False)
-
-    for i in range(10000):
-        r = re.get(url.format(manage), verify=False).text
-
-        if 'Some important content' in r:
-            sleep(i*s%(5<<6))
-            continue
-        break
+        if r.status_code == 200:
+            print('Sucessful ping after being active for {} seconds, the current time is {}.'.format(i*args.seconds, datetime.datetime.now()))
+            sleep(args.seconds)
+        else:
+            print('Unsucessful ping with code', r.status_code)
+            print('Exiting...')
+            exit()
 
 def parse(requestfile):
-    # Parses requestfile, returns a request dictionary for replay.
-    pass
+    with open(requestfile, 'r') as f:
+        file_contents = f.read().splitlines()
+    
+    assert 'GET' in file_contents[0] or 'POST' in file_contents[0], "File does not contain GET or POST request"
 
+    headers = {}
+    data = {}
+    for i in range(len(file_contents))[1:]:
+        if ': ' in file_contents[i]:
+            k, v = file_contents[i].split(': ')
+            headers[k] = v
+            continue
+        if file_contents[i].strip():
+            for param in file_contents[i].split('&'):
+                k, v = param.split('=')
+                data[k] = v
+
+    method = 'GET' in file_contents[0], file_contents[0].split()[1]
+    
+    return method, headers, data
 
 if __name__ == '__main__':
     args = parser.parse_args()
-
-    try:
-        request = parse(args.request)
-    except Exception as e:
-        print("[{}] while parsing file {}".format(e,args.request))
-
     main(args)
